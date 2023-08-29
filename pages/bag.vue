bc3b3fba1a0a75d84685201c51880aa4ddf4811e
maVovk <MichaelVovk@yandex.ru>
2023-08-29 17:20:00 +0300

    Промокоды и страница оплаты

--- pages/bag.vue
+++ pages/bag.vue
@@ -1,8 +1,10 @@
 <template>
   <section>
       <Navbar />
+      <div v-if="!payment">
       <div class="flex justify-center ">
           <div class="w-5/6 max-w-[1196px] flex flex-row justify-between">
+            
             <div class="w-[48%]">
 
                 <div class="text-5xl font-semibold mt-28">
@@ -38,7 +40,7 @@
             </div>
             <div class="w-[48%] pri mt-28 flex flex-col items-center">
                 <div style="cursor:pointer;border: 2px solid black;border-radius:100px;" class="flex justify-center items-center p-4 mt-6 w-[90%]">
-                    <div class="text-5xl font-semibold">
+                    <div class="text-5xl font-semibold" @click="order">
                         Оформить заказ
                     </div>
                 </div>
@@ -46,27 +48,48 @@
                     <div class="flex flex-row justify-between mt-12">
                         <div class="text-2xl font-semibold" >Всего товаров: {{ len.toLocaleString('ru-RU') }} </div>
                         <div class="text-2xl font-semibold">
-                            <div>{{sum1.toLocaleString('ru-RU')}} ₽</div>
+                            <div>{{all_sum.toLocaleString('ru-RU')}} ₽</div>
                         </div>
                     </div>
                     <div class="line mt-7"></div>
                     <div class="flex flex-row justify-between mt-7">
                         <div class="text-2xl font-semibold" >Cкидка</div>
                         <div class="text-2xl font-semibold">
-                            <div>-{{(sum1 - sum).toLocaleString('ru-RU')}} ₽</div>
+                            <div>-{{(all_sum - final_sum).toLocaleString('ru-RU')}} ₽</div>
                         </div>
                     </div>
                     <div class="flex flex-row justify-between mt-[131px]">
                         <div class="text-2xl font-semibold" >Итого</div>
                         <div class="text-2xl font-semibold">
-                            <div>{{ sum.toLocaleString('ru-RU') }} ₽</div>
+                            <div>{{ final_sum.toLocaleString('ru-RU') }} ₽</div>
                         </div>
                     </div>
-                     <input type="text" placeholder="промокод" class="mb-[151px] mt-11 rounded-full h-7 w-full place"> 
+                     <input type="text" id="coupon_input" placeholder="Промокод" class="mb-[151px] mt-11 rounded-full h-7 w-full place coupon"> 
                 </div>
             </div>
             </div>
+            </div>
+
       </div>
+      <div v-else>
+        <div class="flex justify-center ">
+                <div class="w-5/6 max-w-[1196px] flex flex-row justify-between">
+                    <form name="payform-tinkoff" onsubmit="pay(this); return false;">
+                    <input class="payform-tinkoff-row" type="hidden" name="terminalkey" value="TinkoffBankTest">
+                    <input class="payform-tinkoff-row" type="hidden" name="frame" value="false">
+                    <input class="payform-tinkoff-row" type="hidden" name="language" value="ru"> 
+
+                        <input class="mb-[151px] mt-11 mb-0 rounded-full h-7 w-full place" id="order_sum" type="text" placeholder="Сумма заказа" name="amount" required>
+                        <input class="mb-[151px] mt-1 mb-0 rounded-full h-7 w-full place" type="text" placeholder="Номер заказа" name="order">
+                        <input class="mb-[151px] mt-1 mb-0 rounded-full h-7 w-full place" type="text" placeholder="Описание заказа" name="description">
+                        <input class="mb-[151px] mt-1 mb-0 rounded-full h-7 w-full place" type="text" placeholder="ФИО плательщика" name="name">
+                        <input class="mb-[151px] mt-1 mb-0 rounded-full h-7 w-full place" type="text" placeholder="E-mail" name="email">
+                        <input class="mb-[151px] mt-1 mb-0 rounded-full h-7 w-full place" type="text" placeholder="Контактный телефон" name="phone">
+                        <input class="text-5xl font-semibold mb-[151px] mt-7 mb-0 rounded-full h-7 place" type="submit" value="Оплатить">
+                    </form>
+                </div>
+        </div>
+        </div>
       <Under />
   </section>
 </template>
@@ -84,9 +107,11 @@ export default {
     return{
       arrar: raspData,
       hor: [],
-      sum: 0,
+      final_sum: 0,
       len: 0,
-      sum1: 0,
+      all_sum: 0,
+      payment: false,
+      used_coupon: false
     }
   },
   mounted(){
@@ -97,23 +122,26 @@ export default {
             this.hor.push(JSON.parse(localStorage.getItem(i)))
         }
     }
+
     console.log(this.hor);
+
     for(var n=0; n < this.arrar.length + 1; n++){
         if (this.hor[n] == null) {
             console.log('ladno');
         } else {
-            this.sum = this.sum + Number(this.hor[n].price)*Number(this.hor[n].count)
+            this.final_sum = this.final_sum + Number(this.hor[n].price)*Number(this.hor[n].count)
             this.len = this.len + Number(this.hor[n].count)
         }
     }
+
     for(var z=0; z < this.arrar.length + 1; z++){
         if (this.hor[z] == null) {
             console.log('ladno');
         } else {
             if (this.hor[z].priceC == '') {
-                this.sum1 = this.sum1 + Number(this.hor[z].price)*Number(this.hor[z].count)
+                this.all_sum = this.all_sum + Number(this.hor[z].price)*Number(this.hor[z].count)
             } else {
-                this.sum1 = this.sum1 + Number(this.hor[z].priceC)*Number(this.hor[z].count)
+                this.all_sum = this.all_sum + Number(this.hor[z].priceC)*Number(this.hor[z].count)
             }
         }
     }
@@ -126,16 +154,20 @@ export default {
     plus(item){
       let k = document.getElementById(item.id).innerHTML
       let m = Number(k) + 1
+
       document.getElementById(item.id).innerHTML = m
+
       this.arrar[item.id - 1].count = m
       localStorage.removeItem(item.id)
       localStorage.setItem(item.id, JSON.stringify(this.arrar[item.id - 1]));
-      this.sum = this.sum + Number(item.price)
+
+      this.final_sum = this.final_sum + Number(item.price)
       this.len = this.len + 1
+
       if (item.priceC == '') {
-        this.sum1 = this.sum1 + Number(item.price)
+        this.all_sum = this.all_sum + Number(item.price)
       } else {
-        this.sum1 = this.sum1 + Number(item.priceC)
+        this.all_sum = this.all_sum + Number(item.priceC)
       }
     },
     minus(item){
@@ -148,13 +180,60 @@ export default {
             this.arrar[item.id - 1].count = m
             localStorage.removeItem(item.id)
             localStorage.setItem(item.id, JSON.stringify(this.arrar[item.id - 1]));
-            this.sum = this.sum - Number(item.price)
+            this.final_sum = this.final_sum - Number(item.price)
             if (item.priceC == '') {
-                this.sum1 = this.sum1 - Number(item.price)
+                this.all_sum = this.all_sum - Number(item.price)
             } else {
-                this.sum1 = this.sum1 - Number(item.priceC)
+                this.all_sum = this.all_sum - Number(item.priceC)
             }
             this.len = this.len - 1     
+        }
+    },
+    order(){
+        let coupon_text = document.getElementById('coupon_input').value;
+
+        if(coupon_text != '' && !this.used_coupon){
+            fetch(`${process.env.BASE_URL}/coupons/?text=${coupon_text.toUpperCase()}`)
+            .then(response => response.json())
+            .then(coupons => {
+                console.log(coupons);
+                if(coupons.length == 0){
+                    console.log('no such coupon');
+                }
+                if(coupons.length > 1){
+                    console.log('more than one coupon');
+                }
+                else{
+                    let coupon_info = coupons[0];
+
+                    if(coupon_info.type == 'sum'){
+                        this.final_sum -= coupon_info.discount;
+
+                        this.final_sum = Math.max(this.final_sum, 1);
+                    }
+                    else if(coupon_info.type == 'percent'){
+                        this.final_sum -= this.final_sum * coupon_info.discount / 100;
+                    }
+                    else{
+                        console.log('error');
+                    }
+                }
+                this.used_coupon = true;
+
+                this.payment = true;
+                // если убрать, то элемент не будет существовать на момент поиска
+                setTimeout(() => {
+                    document.getElementById('order_sum').value = this.final_sum;
+                }, 1);
+            })
+            .catch(error => console.error(error));
+        }
+        else{
+            this.payment = true;
+            // если убрать, то элемент не будет существовать на момент поиска
+            setTimeout(() => {
+                document.getElementById('order_sum').value = this.final_sum;
+            }, 1);
         }
     }
   },
@@ -199,5 +278,8 @@ input::-webkit-input-placeholder{
 .allCards{
     max-height: 850px;
     overflow-y: scroll;
+}
+.coupon{
+    text-transform: uppercase;
 }
 </style>
